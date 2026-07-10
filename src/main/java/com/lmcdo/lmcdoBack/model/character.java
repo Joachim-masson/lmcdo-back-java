b4b7df34cfb276991b498eaa535d52c33fcf78c5
Joachim MASSON <joachim.masson.17@gmail.com>
2026-07-10 12:15:22 +0200

    ajoute les models

---
+++ src/main/java/com/lmcdo/lmcdoBack/model/character.java
@@ -0,0 +1,32 @@
+package com.lmcdo.lmcdoBack.model;
+
+import jakarta.persistence.Column; 
+import jakarta.persistence.Entity; 
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Id; 
+import jakarta.persistence.Table; 
+
+import lombok.Data; 
+
+@Data 
+@Entity 
+@Table(name = "character") 
+
+public class character {
+  @Id 
+  @GeneratedValue(strategy = GenerationType.IDENTITY) 
+  private Long id; 
+
+  private String name; 
+
+  private int age; 
+
+  private String origin; 
+
+  private String picture; 
+
+  private String created_by;
+
+  private String created_date;
+}
---
+++ src/main/java/com/lmcdo/lmcdoBack/model/logs.java
@@ -0,0 +1,22 @@
+package com.lmcdo.lmcdoBack.model;
+
+import jakarta.persistence.Column; 
+import jakarta.persistence.Entity; 
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Id; 
+import jakarta.persistence.Table; 
+
+import lombok.Data; 
+
+@Data 
+@Entity 
+@Table(name = "logs") 
+
+public class logs {
+  @Id 
+  @GeneratedValue(strategy = GenerationType.IDENTITY) 
+  private Long id; 
+
+  
+}
