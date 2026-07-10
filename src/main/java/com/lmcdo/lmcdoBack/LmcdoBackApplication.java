f168be7e946cfacbfbd124601cb6ad02351a9993
Joachim MASSON <joachim.masson.17@gmail.com>
2026-07-10 11:50:08 +0200

    first commit

---
+++ src/main/java/com/lmcdo/lmcdoBack/LmcdoBackApplication.java
@@ -0,0 +1,13 @@
+package com.lmcdo.lmcdoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmcdoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmcdoBackApplication.class, args);
+	}
+
+}
