e15da5de6d1f108d679d1c25355b7ce3c9e66220
Joachim MASSON <joachim.masson.17@gmail.com>
2026-07-13 15:13:06 +0200

    ajoute l'autorisation cors pour permettre au front de fetcher les données de la table character

--- src/main/java/com/lmcdo/lmcdoBack/controller/CharacterController.java
+++ src/main/java/com/lmcdo/lmcdoBack/controller/CharacterController.java
@@ -3,6 +3,7 @@ package com.lmcdo.lmcdoBack.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.lmcdo.lmcdoBack.model.Character;
 import com.lmcdo.lmcdoBack.service.CharacterService;
 
 @RestController // permet d’indiquer à Spring que cette classe est un bean.Elle indique à Spring d’insérer le retour de la méthode au format JSON dans le corps de la réponse HTTP
+@CrossOrigin(origins = "http://localhost:5173")
 public class CharacterController {
   @Autowired
   private CharacterService characterService;
